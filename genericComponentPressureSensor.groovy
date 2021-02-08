5fb6e30c3373a23d663c5ed3586a26bcaa3e21be
tomwpublic <69065662+tomwpublic@users.noreply.github.com>
2021-02-08 09:24:58 -0600

    Update and rename genericComponentPressureSensor to genericComponentPressureSensor.groovy

--- genericComponentPressureSensor.groovy
+++ genericComponentPressureSensor.groovy
@@ -24,7 +24,7 @@ Change history:
 
 metadata
 {
-    definition(name: "Generic Component Pressure Sensor", namespace: "community", author: "tomw", importUrl: "")
+    definition(name: "Generic Component Pressure Sensor", namespace: "community", author: "tomw", importUrl: "https://raw.githubusercontent.com/ymerj/HE-HA-control/main/genericComponentPressureSensor.groovy")
     {
         capability "PressureMeasurement"
     }

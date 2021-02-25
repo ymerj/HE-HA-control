225ad549f61dc8e14c32e6d77fb3681a0d45b8cb
tomwpublic <69065662+tomwpublic@users.noreply.github.com>
2021-02-24 20:02:29 -0600

    Update haDeviceBridgeConfiguration.groovy

--- haDeviceBridgeConfiguration.groovy
+++ haDeviceBridgeConfiguration.groovy
@@ -1,11 +1,26 @@
 /*
-
-* 0.1.22     2021-02-24 tomw               Entity filtering (in work)
-* 0.1.221    2021-02-24 tomw               Usability via friendly_name on filtering and select/clear all
-* 0.1.222    2021-02-24 tomw               Polishing usability
-* 0.1.223    2021-02-24 tomw               Cosmetic changes and configure driver parameters from app
-
- */
+* Home Assistant to Hubitat Integration
+*
+* Description:
+* Allow control of HA devices.
+*
+* Required Information:
+* Home Asisstant IP and Port number
+* Home Assistant long term Access Token
+*
+* Features List:
+*
+* Licensing:
+* Copyright 2021 tomw
+* Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+* in compliance with the License. You may obtain a copy of the License at: http://www.apache.org/licenses/LICENSE-2.0
+* Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+* on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+* for the specific language governing permissions and limitations under the License.
+*
+* Version Control:
+* 0.1.22     2021-02-24 tomw               Optional configuration app to selectively filter out Home Assistant devices
+*/
 
 definition(
     name: "Home Assistant Device Bridge configuration",

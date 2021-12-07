6e28f40f13ba3fb00ab8861d254f711653be0724
draperw <bill.draper@gmail.com>
2021-12-06 22:09:31 -0600

    Update haDeviceBridgeConfiguration.groovy

--- haDeviceBridgeConfiguration.groovy
+++ haDeviceBridgeConfiguration.groovy
@@ -32,8 +32,8 @@ definition(
     category: "Convenience",
     iconUrl: "",
     iconX2Url: "",
-    iconX3Url: ""),
-    importUrl: "https://raw.githubusercontent.com/ymerj/HE-HA-control/main/HA%20parent.groovy",
+    iconX3Url: "",
+    importUrl: "https://raw.githubusercontent.com/ymerj/HE-HA-control/main/HA%20parent.groovy")
 
 preferences
 {

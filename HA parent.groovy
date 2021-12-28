e6fea31b9e556a4279679d38cf7e75f8d4efb853
ymerj <63329762+ymerj@users.noreply.github.com>
2021-12-27 23:40:21 -0500

    Update HA parent.groovy

--- HA parent.groovy
+++ HA parent.groovy
@@ -240,10 +240,10 @@ def parse(String description) {
                 break
             case "climate":
                 def current_temperature = response?.event?.data?.new_state?.attributes?.current_temperature
-                // def thermostat_state = response?.event?.data?.new_state?.attributes?.hvac_action
+                def hvac_action = response?.event?.data?.new_state?.attributes?.hvac_action
                 def target_temperature = response?.event?.data?.new_state?.attributes?.temperature
                 def fan_mode = response?.event?.data?.new_state?.attributes?.fan_mode
-                def thermostat_mode = response?.event?.data?.new_state?.attributes?.hvac_mode
+                def thermostat_mode = response?.event?.data?.new_state?.state
                 switch (fan_mode)
                     case "off":
                         thermostat_mode = "off"
@@ -263,11 +263,12 @@ def parse(String description) {
                     case "dry":
                     case "auto":
                        return
-                
+            
+                newVals[0] = thermostat_mode  
                 newVals += current_temperature
                 newVals += target_temperature
                 newVals += fan_mode
-                newVals += thermostat_mode                
+                newVals += hvac_action                
                 mapping = translateDevices(domain, newVals, friendly, origin)
                 if (mapping) updateChildDevice(mapping, entity, friendly) 
                 break
@@ -306,7 +307,7 @@ def translateDevices(device_class, newVals, friendly, origin)
             device_tracker: [type: "Generic Component Presence Sensor", event: [[name: "presence", value: newVals[0] == "home" ? "present":"not present", descriptionText:"${friendly} is ${newVals[0]}"]], namespace: "community"],
             cover: [type: "Generic Component Garage Door Control",      event: [[name: "door", value: newVals[0] ?: "unknown", type: origin, descriptionText:"${friendly} was turn ${newVals[0]} [${origin}]"]], namespace: "community"],
             lock: [type: "Generic Component Lock",                      event: [[name: "lock", value: newVals[0] ?: "unknown", type: origin, descriptionText:"${friendly} was turn ${newVals[0]} [${origin}]"]]],
-            climate: [type: "Generic Component Thermostat",             event: [[name: "thermostatMode", value: newVals[0], descriptionText: "${friendly} is set to ${newVals[0]}"],[name: "temperature", value: newVals[1], descriptionText: "${friendly}'s current temperature is ${newVals[1]} degree"],[name: "coolingSetpoint", value: newVals[2], descriptionText: "${friendly}'s cooling temperature is set to ${newVals[2]} degree"],[name: "heatingSetpoint", value: newVals[2], descriptionText: "${friendly}'s heating temperature is set to ${newVals[2]} degree"],[name: "thermostatFanMode", value: newVals[3], descriptionText: "${friendly}'s fan is set to ${newVals[3]}"],[name: "thermostatSetpoint", value: newVals[2], descriptionText: "${friendly}'s temperature is set to ${newVals[2]} degree"],[name: "thermostatMode", value: newVals[4], descriptionText: "${friendly}'s mode is set to ${newVals[4]}"]]]
+            climate: [type: "Generic Component Thermostat",             event: [[name: "thermostatMode", value: newVals[0], descriptionText: "${friendly} is set to ${newVals[0]}"],[name: "temperature", value: newVals[1], descriptionText: "${friendly}'s current temperature is ${newVals[1]} degree"],[name: "coolingSetpoint", value: newVals[2], descriptionText: "${friendly}'s cooling temperature is set to ${newVals[2]} degree"],[name: "heatingSetpoint", value: newVals[2], descriptionText: "${friendly}'s heating temperature is set to ${newVals[2]} degree"],[name: "thermostatFanMode", value: newVals[3], descriptionText: "${friendly}'s fan is set to ${newVals[3]}"],[name: "thermostatSetpoint", value: newVals[2], descriptionText: "${friendly}'s temperature is set to ${newVals[2]} degree"],[name: "thermostatOperatingState", value: newVals[4], descriptionText: "${friendly}'s mode is ${newVals[4]}"]]]
         ]
 
     return mapping[device_class]

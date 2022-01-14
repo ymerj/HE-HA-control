f951f05262b35e7002e84bfdfb9fca351fd0e77b
ymerj <63329762+ymerj@users.noreply.github.com>
2022-01-14 12:42:06 -0500

    Update HA parent.groovy

--- HA parent.groovy
+++ HA parent.groovy
@@ -549,18 +549,28 @@ def componentSetThermostatMode(ch, thermostatmode){
 def componentSetCoolingSetpoint(ch, temperature){
     if (logEnable) log.info("received setCoolingSetpoint request from ${ch.label}")
 
-    data = [temperature: temperature, hvac_mode: "cool"]
+    if (ch.currentValue("thermostatMode") == "auto") {
+        data = [target_temp_high: temperature, target_temp_low: ch.currentValue("heatingSetpoint"), hvac_mode: "heat_cool"]
+	}
+    else {
+	data = [temperature: temperature, hvac_mode: "cool"]
+	}
     executeCommand(ch, "set_temperature", data)
 }
 
-def componentSetHeatingSetpoint(ch, temperature){
+def componentSetHeatingSetpoint(ch, temperature) {
     if (logEnable) log.info("received setHeatingSetpoint request from ${ch.label}")
 
-    data = [temperature: temperature, hvac_mode: "heat"]
+    if (ch.currentValue("thermostatMode") == "auto") {
+	data = [target_temp_high: ch.currentValue("coolingSetpoint"), target_temp_low: temperature, hvac_mode: "heat_cool"]
+    }
+    else {
+	data = [temperature: temperature, hvac_mode: "heat"]
+    }
     executeCommand(ch, "set_temperature", data)
 }
 
-def componentSetThermostatFanMode(ch, fanmode){
+def componentSetThermostatFanMode(ch, fanmode) {
     if (logEnable) log.info("received fanmode request from ${ch.label}")
 
     if (fanmode == "circulate") {

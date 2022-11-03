847d4848c8c5a36eb6e10528529fffb1fbfe0579
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2022-11-03 16:21:27 -0400

    Update genericComponentCarbonDioxideSensor.groovy

--- genericComponentCarbonDioxideSensor.groovy
+++ genericComponentCarbonDioxideSensor.groovy
@@ -1,6 +1,6 @@
 /*
 
-Copyright 2021
+Copyright 2022
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -24,7 +24,7 @@ Change history:
 
 metadata
 {
-    definition(name: "Generic Component Carbon Dioxide Sensor", namespace: "community", author: "community", importUrl: "https://raw.githubusercontent.com/ymerj/HE-HA-control/main/genericComponentCarbonDioxideSensor.groovy")
+    definition(name: "Generic Component Carbon Dioxide Sensor", namespace: "community", author: "community", importUrl: "https://raw.githubusercontent.com/mboisson/HE-HA-control/airthings/genericComponentCarbonDioxideSensor.groovy")
     {
         capability "CarbonDioxideMeasurement"
         capability "Refresh"

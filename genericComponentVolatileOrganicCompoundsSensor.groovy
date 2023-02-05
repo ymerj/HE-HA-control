d5048ec88e7167be0f40fb10ac33ee52aab77f2f
ymerj <63329762+ymerj@users.noreply.github.com>
2023-02-05 13:47:31 -0500

    Update genericComponentVolatileOrganicCompoundsSensor.groovy

--- genericComponentVolatileOrganicCompoundsSensor.groovy
+++ genericComponentVolatileOrganicCompoundsSensor.groovy
@@ -27,11 +27,13 @@ metadata
     definition(name: "Generic Component Volatile Organic Compounds Sensor", namespace: "community", author: "community", importUrl: "https://raw.githubusercontent.com/mboisson/HE-HA-control/airthings/genericComponentVolatileOrganicCompoundsSensor.groovy")
     {
         capability "Refresh"
+        capability "Health Check"
     }
     preferences {
         input name: "txtEnable", type: "bool", title: "Enable descriptionText logging", defaultValue: true
     }
     attribute "voc", "number"
+    attribute "healthStatus", "enum", ["offline", "online"]
 }
 
 void updated() {
@@ -55,6 +57,7 @@ void parse(List<Map> description) {
     description.each {
         if (it.name in ["volatile_organic_compounds"]) {
             if (txtEnable) log.info it.descriptionText
+            it.value == "unavailable" ? offline() : online()
             unit="ppb"
             updateAttr("voc", it.value, unit)
             sendEvent(it)
@@ -64,4 +67,16 @@ void parse(List<Map> description) {
 
 void refresh() {
     parent?.componentRefresh(this.device)
+}
+
+def offline() {
+    sendEvent(name: "healthStatus", value: "offline")
+}
+
+def online() {
+    sendEvent(name: "healthStatus", value: "online")
+}
+
+void ping() {
+    refresh()
 }

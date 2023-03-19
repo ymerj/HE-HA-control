06c5f4c4c1c600b89c1405c26c151fa0e1615a94
ymerj <63329762+ymerj@users.noreply.github.com>
2023-03-19 16:12:38 -0400

    Update genericComponentMediaPlayer.groovy

--- genericComponentMediaPlayer.groovy
+++ genericComponentMediaPlayer.groovy
@@ -64,7 +64,7 @@ void parse(List<Map> description) {
             sendEvent(name: "healthStatus", value: it.value == "unavailable" ? "offline" : "online")
             sendEvent(it)
         }
-        if (it.name in ["switch", "status", "mute", "volume", "mediaInputSource", "supportedInputs"]) {
+        if (it.name in ["switch", "mute", "volume", "mediaInputSource", "supportedInputs"]) {
             if (txtEnable) log.info it.descriptionText
             sendEvent(it)
         }

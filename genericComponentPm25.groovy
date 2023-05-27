e168b5203554ae9bd339fc657d92d6320e66792f
ymerj <63329762+ymerj@users.noreply.github.com>
2023-05-27 15:30:44 -0400

    Create genericComponentPm25.groovy

---
+++ genericComponentPm25.groovy
@@ -0,0 +1,76 @@
+/*
+
+Copyright 2023
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+-------------------------------------------
+
+Change history:
+
+0.1.55 - Yves Mercier - initial version
+
+*/
+
+metadata
+    {
+    definition(name: "Generic Component Pm25", namespace: "community", author: "community", importUrl: "https://raw.githubusercontent.com/ymerj/HE-HA-control/main/genericComponentPm25.groovy")
+        {
+        capability "Refresh"
+        capability "Health Check"
+        }
+    preferences
+        {
+        input name: "txtEnable", type: "bool", title: "Enable descriptionText logging", defaultValue: true
+        }
+    attribute "pm25", "number"
+    attribute "healthStatus", "enum", ["offline", "online"]
+    }
+
+void updated()
+    {
+    log.info "Updated..."
+    log.warn "description logging is: ${txtEnable == true}"
+    }
+
+void installed()
+    {
+    log.info "Installed..."
+    device.updateSetting("txtEnable",[type:"bool",value:true])
+    refresh()
+    }
+
+void parse(String description) { log.warn "parse(String description) not implemented" }
+
+void parse(List<Map> description)
+    {
+    description.each
+        {
+        if (it.name in ["pm25"])
+            {
+            if (txtEnable) log.info it.descriptionText
+            sendEvent(name: "healthStatus", value: it.value == "unavailable" ? "offline" : "online")
+            sendEvent(it)
+            }
+        }
+    }
+
+void refresh()
+    {
+    parent?.componentRefresh(this.device)
+    }
+
+void ping()
+    {
+    refresh()
+    }

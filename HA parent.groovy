273abacb3a6b91930d21d3ace5722b0044adbef8
ymerj <63329762+ymerj@users.noreply.github.com>
2024-01-29 16:25:00 -0500

    Update HA parent.groovy

--- HA parent.groovy
+++ HA parent.groovy
@@ -346,7 +346,7 @@ def parse(String description) {
                 def target_temp_high = newState?.attributes?.target_temp_high
                 def target_temp_low = newState?.attributes?.target_temp_low
 		def hvac_modes = []
-                def hvac_modes = newState?.attributes?.hvac_modes
+                hvac_modes = newState?.attributes?.hvac_modes
 		log.info "untouch ${hvac_modes}"
                 hvac_modes = hvac_modes.minus(["auto", "dry", "fan_only"])
 		log.info "minus ${hvac_modes}"

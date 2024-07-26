1f142c99fd195c19b1fec18a7ba76829cd0c02f2
ymerj <63329762+ymerj@users.noreply.github.com>
2024-07-26 07:13:12 -0400

    Update HA parent.groovy

--- HA parent.groovy
+++ HA parent.groovy
@@ -88,7 +88,7 @@
 * 2.4    2024-04-27 Yves Mercier       Add humidity to climate entity
 * 2.5    2024-05-24 Yves Mercier       Add support for valve entity and add supported fan modes for climate entity
 * 2.6    2024-06-11 Yves Mercier       Add support for humidifier entity
-* 2.7    2024-06-29 Yves Mercier       Add support for events, change fan error handling, remap fan percentage to accomodate for missing named speed.
+* 2.7    2024-06-29 Yves Mercier       Add support for events, change fan error handling, remap fan percentage to accomodate for missing named speed, forgo thermostat mode translation, add thermostat presets.
 */
 
 import groovy.json.JsonSlurper

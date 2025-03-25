87dea7c22aa161d4ec7b29a7ee7546252bd2b7b4
ymerj <63329762+ymerj@users.noreply.github.com>
2025-03-25 15:20:53 -0400

    Update haDeviceBridgeConfiguration.groovy

--- haDeviceBridgeConfiguration.groovy
+++ haDeviceBridgeConfiguration.groovy
@@ -37,7 +37,8 @@
 * 2.10       2024-11-12 Yves Mercier       Add support for text and vacuums
 * 2.11       2024-11-30 Yves Mercier       Add limited support for media_player entity
 * 2.12       2024-12-15 Yves Mercier       Add support for select entity
-* 2.15       2024-01-17 Yves Mercier       Fix "Toggle all On/Off" included as an entity
+* 2.15       2025-01-17 Yves Mercier       Fix "Toggle all On/Off" included as an entity
+* 2.18       2025-03-24 Yves Mercier       Add support for sirens
 */
 
 definition(
